95f3c0d5cad4b46bc283983bf460a19d99c1d0d3
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-02-09 03:04:44 +0530

    Added app.scss and imported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import './app.scss'
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>HERO</section>
+    <section>PARALLAX</section>
+    <section>SERVICES</section>
+    <section>PARALLAX</section>
+    <section>PORTFOLIO1</section>
+    <section>PORTFOLIO2</section>
+    <section>PORTFOLIO3</section>
+    <section>CONTACT</section>
+  </div>;
 };
 
 export default App;
